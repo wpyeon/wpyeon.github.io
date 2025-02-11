beee84e570b889b58c02b5c0d7df3391ecbdcb91
Woojang Pyeon <woojangpyeon@gmail.com>
2025-02-11 17:20:14 +0800

    fix: make MediaGridItems in the same row to have uniform height

--- src/components/MediaGridItem.tsx
+++ src/components/MediaGridItem.tsx
@@ -8,13 +8,13 @@ export interface MediaGridItemProps {
 
 export default function MediaGridItem({ imgSrc, title, chipTypes }: MediaGridItemProps) {
   return (
-    <div className="flex flex-col rounded-lg overflow-hidden bg-white shadow-lg desktop:hover:-translate-y-1 desktop:hover:-translate-x-1 desktop:transition-transform desktop:duration-300">
+    <div className="flex flex-col h-full rounded-lg overflow-hidden bg-white shadow-lg desktop:hover:-translate-y-1 desktop:hover:-translate-x-1 desktop:transition-transform desktop:duration-300">
       <img 
         className="w-full h-[200px] object-cover mb-0" 
         src={imgSrc} 
         alt="Project Image" 
       />
-      <div className="flex flex-col p-4 mt-0">
+      <div className="flex flex-col flex-1 justify-between p-4 mt-0">
         <div className="text-base font-semibold text-gray-800 mb-2">
           {title ? title : "Project"}
         </div>
